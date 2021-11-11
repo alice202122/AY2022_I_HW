743f9e28edfdb220f94405b15782c9dd615d7de8
alice <alice.savino@mail.polimi.it>
2021-11-11 19:25:40 +0100

    code comments

--- GROUP_03.cydsn/InterruptRoutines.c
+++ GROUP_03.cydsn/InterruptRoutines.c
@@ -3,16 +3,16 @@
 * for the interrupt service routines.
 */
 
-// Include required header files
 #include "InterruptRoutines.h" 
 #include "project.h"
-int flagTEMP, flagLDR;
-extern int32 value_digit[];
+int flagTEMP, flagLDR; //flags used to detect the status bits of each channel
+
 int32 media_TEMP, media_LDR;
 
 // Our global variables
-extern uint8_t slaveBuffer[];
 
+extern int32 value_digit[]; // used to store the results of the A/D conversion
+extern uint8_t slaveBuffer[];
 extern uint8_t slaveCounter;
 
     
@@ -21,29 +21,29 @@ CY_ISR(Custom_Timer_Count_ISR)
 {
     slaveCounter++;
     // Read timer status register to pull interrupt line low
-    Timer_ADC_ReadStatusRegister();
-    if (( flagLDR==1) & (flagTEMP==1))
+    Timer_ADC_ReadStatusRegister(); // used  to read the status register value
+    //1 case: Temperature and Light signals are sampled
+    if (( flagLDR==1) & (flagTEMP==1)) // both channels are set to 1
     {
-        
-        
-        if (slaveCounter==0) AMux_Select(0);
+        if (slaveCounter==0) AMux_Select(0); // to check that if the counter=0 the multiplexer is setted to the first channel
        
-        value_digit[slaveCounter] = ADC_DelSig_Read32();
-        /*if (value_digit[slaveCounter] < 0) value_digit[slaveCounter] = 0;
-        if (value_digit[slaveCounter] > 65535) value_digit[slaveCounter] = 65535;*/
+       
+        value_digit[slaveCounter] = ADC_DelSig_Read32(); // The ADC is a 16-bit single ended mode so we use the ADC_Read32() function to make the conversion
+        if (value_digit[slaveCounter] < 0) value_digit[slaveCounter] = 0; //check of the minimum and maximum positive value allowed for a 16 bit ADC
+        if (value_digit[slaveCounter] > 65535) value_digit[slaveCounter] = 65535;
                 
-        if (slaveCounter == 4)
+        if (slaveCounter == 4) // as required, when the first 5 samples are collected, we compute the mean
         {  
             for( int	i	=	0;	i < 5; i++ ) 
             {
                 media_TEMP += (value_digit[i]);
             } 
             media_TEMP= media_TEMP/5;
-            AMux_Select(1);
+            AMux_Select(1); // at this point we switch to the 2 channel of the multiplexer to acquire the light signal
             
         }
         
-        if (slaveCounter== 9)
+        if (slaveCounter== 9) //when the last 5 samples are collected, we compute the mean 
         {
             value_digit[slaveCounter] = ADC_DelSig_Read32();
             for (int i= 5;  i<10; i++)
@@ -51,20 +51,22 @@ CY_ISR(Custom_Timer_Count_ISR)
                     media_LDR += (value_digit[i]);
                 }
                 media_LDR= media_LDR/5;
-                slaveBuffer[3] = media_TEMP>>8;
-               
-                slaveBuffer[4] = media_TEMP & 0xFF;
-                slaveBuffer[5] = media_LDR>>8;
-                slaveBuffer[6] = media_LDR & 0xFF;
-                AMux_Select(0);
-                slaveCounter=-1;     
+                //we save the data relative to the sample acquisition in the dedicate SlaveBuffer register addresses
+                slaveBuffer[3] = media_TEMP>>8; //We store the most significant Temperature related Bits in the 0x03 register 
+                slaveBuffer[4] = media_TEMP & 0xFF;  //We store the least significant Temperature related Bits in the 0x04 register
+                slaveBuffer[5] = media_LDR>>8; //We store the most significant Light related Bits in the 0x05 register 
+                slaveBuffer[6] = media_LDR & 0xFF; //We store the least significant Light related Bits in the 0x06 register 
+                AMux_Select(0); // at this point we switch to the 1 channel of the multiplexer in order to restart the data acquisition
+                slaveCounter=-1; // we reset the counter    
         } 
     }
     
-    //2 condizione
+    //2 case: Only temperature signals are sampled
     
-    if (( flagLDR==0) & (flagTEMP==1))
+    if (( flagLDR==0) & (flagTEMP==1)) // Channel 0 is set to 1, channel 1 is set to 0.
     {
+        slaveBuffer[5]= 0;
+        slaveBuffer[6] =0;
         value_digit[slaveCounter] = ADC_DelSig_Read32();
         if (slaveCounter == 4)
         {     
@@ -78,9 +80,11 @@ CY_ISR(Custom_Timer_Count_ISR)
             slaveCounter=-1;
             }
     }
-    //3 condizione
-    if (( flagLDR==1) & (flagTEMP==0))
+    //3 case: Only light signals are sampled
+    if (( flagLDR==1) & (flagTEMP==0)) // Channel 0 is set to 0, channel 1 is set to 1
     {
+        slaveBuffer[3]= 0;
+        slaveBuffer[4] =0;
         AMux_Select(1);
         value_digit[slaveCounter] = ADC_DelSig_Read32();
         if (slaveCounter == 4)
@@ -107,26 +111,26 @@ CY_ISR(Custom_Timer_Count_ISR)
 */
 void EZI2C_ISR_ExitCallback(void){
 
-    if ((slaveBuffer[0] & 0b11) == 0b11)
-    
+    if ((slaveBuffer[0] & 0b11) == 0b11) //when the status bits of the ControlRegister0 are both 1 the Led is on
+
     {         
-        Pin_LED_Write(1); 
+        Pin_LED_Write(1); // function that writes the value to the phisical port
         flagTEMP=1;
         flagLDR=1;        
     }
-    if ((slaveBuffer[0] & 0b11)==0b00)
+    if ((slaveBuffer[0] & 0b11)==0b00) //when the status bits of the ControlRegister0 are both 0, the Led is off
     {   
         Pin_LED_Write(0);
         flagTEMP= 0;
         flagLDR=0;
     }
-    if ((slaveBuffer[0]& 0b11)==0b01)
+    if ((slaveBuffer[0]& 0b11)==0b01) //when the status bits of the ControlRegister0 are  0 and 1  the Led is off
     {
         Pin_LED_Write(0);
         flagLDR=0;
         flagTEMP=1;
     }
-     if ((slaveBuffer[0]& 0b11)==0b10)
+     if ((slaveBuffer[0]& 0b11)==0b10) ///when the status bits of the ControlRegister0 are 1 and 0  the Led is off
     {
         Pin_LED_Write(0);
         flagLDR=1;
--- GROUP_03.cydsn/main.c
+++ GROUP_03.cydsn/main.c
@@ -12,24 +12,27 @@
 #include "project.h"
 #include "InterruptRoutines.h"
 #include "stdio.h"
-#define slaveBuffer_size 7
-uint8_t slaveBuffer[slaveBuffer_size];
-int32 value_digit[10];
+#define slaveBuffer_size 7 //our EZI2C is composed by 7 registers
+uint8_t slaveBuffer[slaveBuffer_size]; 
+int32 value_digit[10]; 
 uint8_t slaveCounter;
 
 int main(void)
 {
     CyGlobalIntEnable; /* Enable global interrupts. */
-    
-    isr_StartEx(Custom_Timer_Count_ISR);
+    /* Start the components */    
+    isr_StartEx(Custom_Timer_Count_ISR); 
     AMux_Start();
     AMux_Select(0);
     ADC_DelSig_Start();
     Timer_ADC_Start();
     EZI2C_Start();
-    slaveBuffer[2] = 0xBC;
-    slaveCounter=-1;
-    EZI2C_SetBuffer1(slaveBuffer_size, 2, slaveBuffer);
+    slaveBuffer[2] = 0xBC; //WHO AM I register of the slave buffer set to 0xBC
+    slaveBuffer[1]= 20; //Timer period considering 5 samples (computed as: clock_frequency/n_samples*transmission frequency)
+    //in our case n_samples =5*2 (since we have two signals), clock_frequency = 10kHz, transmission_freq = 50Hz)
+    slaveCounter=-1; 
+    EZI2C_SetBuffer1(slaveBuffer_size, 2, slaveBuffer); //Sets up the data buffer to be exposed to the master on a primary slave address request. 
+    //as arguments of this function we pass the slave buffer size (7), the number of register on which it is possible to both, read and write, and the slave buffer variable.
 
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
 
